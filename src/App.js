9e93ffa5ceef26c15672c226842bc99c5d0166f9
Martin Galli <martin.ggalli@gmail.com>
2019-08-13 14:57:08 -0300

    Burger Builder + Controls + Burger

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import classes from './App.css'
+
+// IMPORT COMPONENTS
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
     render () {
         return (
-            <div className={classes.App}>
-              
+            <div>
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
             </div>
         )
     }
