d8ba0f75971ab5aa24e407a4152ea119a4e2bf71
Martin Galli <martin.ggalli@gmail.com>
2019-08-14 17:58:30 -0300

    Error Handling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummery from '../../components/Burger/OrderSummery/OrderSummery'
 import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 import axios from '../../axios-orders'
 
@@ -104,7 +105,7 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'Fastest'
         }
-        
+
         axios.post('/orders.json', order)
             .then(response => {
                 this.setState({loading: false, purchasing: false})
@@ -152,4 +153,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder
+export default withErrorHandler(BurgerBuilder, axios)
