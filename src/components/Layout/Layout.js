53b20a0baf53b25d60f836ea2137f49f67bc7bc4
Martin Galli <martin.ggalli@gmail.com>
2019-08-14 12:20:03 -0300

    Navigation Items + Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './Layout.css'
 import Aux from '../../hoc/Auxiliar/Auxiliar'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = props => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
 
-export default layout
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer 
+                    closed={this.sideDrawerClosedHandler} 
+                    open={this.state.showSideDrawer}/>
+                <main className={classes.Content}>
+                    {this.props.children} 
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
