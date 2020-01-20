827683feeffc56b5870a9cd707106a6455c411a0
Martin Galli <martin.ggalli@gmail.com>
2020-01-20 15:39:04 -0300

    Authenticate with Google

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavItem link="/" exact>Burger Builder</NavItem>
         <NavItem link="/orders">Orders</NavItem>
+        <NavItem link="/auth">Authenticate</NavItem>
     </ul>
 )
 
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import burgerBuilderReducer from './burgerBuilderReducer';
 import orderReducer from './orderReducer';
+import authReducer from './authReducer';
 
 const initialState = {};
 
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
     main: mainReducer,
     burger: burgerBuilderReducer,
     order: orderReducer,
+    auth: authReducer,
 });
 
 export default rootReducer;
