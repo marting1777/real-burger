1b548add79b68e3d2cebd664671a0701d56dbfd8
Martin Galli <martin.ggalli@gmail.com>
2020-01-22 15:50:15 -0300

    Adding authentication completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
 // IMPORT COMPONENTS
 import Layout from './hoc/Layout/Layout';
@@ -18,15 +18,32 @@ class App extends Component {
     }
 
     render () {
+
+        let routes = (
+            <>
+                <Route path="/auth" component={Auth}/>
+                <Route path="/" exact component={BurgerBuilder}/>
+                <Redirect to="/"/>
+            </>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <>
+                    <Route path="/checkout" component={Checkout}/>
+                    <Route path="/orders" component={Orders}/>
+                    <Route path="/logout" component={Logout}/>
+                    <Route path="/" exact component={BurgerBuilder}/>
+                    <Redirect to="/" />
+                </>
+            );
+        }
+
         return (
             <div>
                 <Layout>
                     <Switch>    
-                        <Route path="/checkout" component={Checkout}/>
-                        <Route path="/orders" component={Orders}/>
-                        <Route path="/auth" component={Auth}/>
-                        <Route path="/logout" component={Logout}/>
-                        <Route path="/" exact component={BurgerBuilder}/>
+                        {routes}
                     </Switch>
                 </Layout>
             </div>
@@ -34,10 +51,16 @@ class App extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null,
+    };
+};
+
 const mapDispatchToProps = dispatch => {
     return {
         onTryAutoSignUp: () => dispatch(authCheckState()),
     };
 };
 
-export default withRouter(connect( null, mapDispatchToProps )( App ));
+export default withRouter(connect( mapStateToProps, mapDispatchToProps )( App ));
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -108,6 +108,7 @@ class ContactData extends Component {
             ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData,
+            userId: this.props.userId,
         }
 
         this.props.onOrderBurger(order, this.props.token);
@@ -198,6 +199,7 @@ const mapStateToProps = state => {
         price: state.burger.totalPrice,
         loading: state.order.loading,
         token: state.auth.token,
+        userId: state.auth.userId,
     };
 };
 
